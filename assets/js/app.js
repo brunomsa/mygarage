db6fcb0ea85f78072eaf6b18276ac250ad0e86c2
Bruno Andrade <brunoandrade99@gmail.com>
2021-10-11 17:49:17 -0300

    roll back sw

--- assets/js/app.js
+++ assets/js/app.js
@@ -824,7 +824,6 @@ const saveCarList = () =>
 const saveWishList = () =>
   localStorage.setItem("arrayWishes", JSON.stringify(arrayWishes));
 
-  
 //Registrar o service worker na aplicação
 if ("serviceWorker" in navigator) {
   // Registra um service worker hospeadado na raiz do
@@ -838,5 +837,5 @@ if ("serviceWorker" in navigator) {
       console.log("Falha ao Registrar o Service Worker:", error);
     });
 } else {
-  console.log("Service workers não suportado!"); 
+  console.log("Service workers não suportado!");
 }
--- sw.js
+++ sw.js
@@ -1,33 +1,34 @@
 // Define o nome do cache atual, considerando a sua versão.
-var cacheName = "mygarage-v1.0";
+var cacheName = 'mygarage-v1.0';
 
 // Armazena todos os arquivos no cache atual
-self.addEventListener("install", function (event) {
+self.addEventListener('install', function (event) {
   caches.open(cacheName).then((cache) => {
     cache.addAll([
-      "/",
-      "/index.html",
-      "/manifest.webmanifest",
-      "/assets/js/app.js",
-      "/assets/css/style.css",
-      "/assets/icons/favicon.ico",
-      "/assets/icons/android-icon-48x48.png",
-      "/assets/icons/android-icon-72x72.png",
-      "/assets/icons/android-icon-96x96.png",
-      "/assets/icons/android-icon-144x144.png",
-      "/assets/icons/android-icon-192x192.png",
-      "/assets/icons/apple-icon-72x72.png",
-      "/assets/icons/apple-icon-120x120.png",
-      "/assets/icons/apple-icon-144x144.png",
-      "/assets/icons/apple-icon-152x152.png",
-      "/assets/icons/apple-icon-180x180.png",
+      '/',
+      '/index.html',
+      '/manifest.webmanifest',
+      '/assest/css/style.css',
+      '/app.js',
+      '/assets/icons/favicon.ico',
+      '/assets/icons/android-icon-48x48.png',
+      '/assets/icons/android-icon-72x72.png',
+      '/assets/icons/android-icon-96x96.png',
+      '/assets/icons/android-icon-144x144.png',
+      '/assets/icons/android-icon-192x192.png',
+      '/assets/icons/apple-icon-72x72.png',
+      '/assets/icons/apple-icon-120x120.png',
+      '/assets/icons/apple-icon-144x144.png',
+      '/assets/icons/apple-icon-152x152.png',
+      '/assets/icons/apple-icon-180x180.png',
     ]);
   });
 });
 
+
 // Recupera todos os nomes de cache e apaga aqueles
 // que forem diferentes do cache atual
-self.addEventListener("activate", (e) => {
+self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
@@ -44,9 +45,9 @@ self.addEventListener("activate", (e) => {
 // Tenta servir o arquivo do cache atual. Se não for possível,
 // baixa o recurso da web e o armazena localmente, antes de entregar
 // uma cópia para o usuário.
-self.addEventListener("fetch", function (event) {
+self.addEventListener('fetch', function (event) {
   if (!(event.request.url.indexOf('http') === 0)) return;
-
+  
   let resposta = caches.open(cacheName).then((cache) => {
     return cache.match(event.request).then((recurso) => {
       if (recurso) return recurso;
