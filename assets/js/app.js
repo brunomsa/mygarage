6eb576be0406a8a3e19a5753ffa2169fffd06e58
Bruno Andrade <brunoandrade99@gmail.com>
2021-10-11 12:55:28 -0300

    edit scope sw

--- assets/js/app.js
+++ assets/js/app.js
@@ -782,5 +782,16 @@ const saveCarList = () =>
 const saveWishList = () =>
   localStorage.setItem("arrayWishes", JSON.stringify(arrayWishes));
 
+  
 //Registrar o service worker na aplicação
-navigator.serviceWorker.register("./app-sw.js");
+if ('serviceWorker' in navigator) {
+  // Registre um service worker hospeado na raiz do
+  // site usando um escopo mais restritivo.
+  navigator.serviceWorker.register('/sw.js', {scope: './'}).then(function(registration) {
+    console.log('Service worker registrado com sucesso:', registration);
+  }).catch(function(error) {
+    console.log('Service worker falhou ao registrar:', error);
+  });
+} else {
+  console.log('Service workers não é suportado pelo navegador!.');
+}
