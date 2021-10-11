5031b94643c1aee72b49dd159dab4f1f9ba280d0
Bruno Andrade <brunoandrade99@gmail.com>
2021-10-11 13:02:25 -0300

    edit sw

--- assets/js/app.js
+++ assets/js/app.js
@@ -783,14 +783,5 @@ const saveWishList = () =>
   localStorage.setItem("arrayWishes", JSON.stringify(arrayWishes));
 
 //Registrar o service worker na aplicação
-if ('serviceWorker' in navigator) {
-  // Registra um service worker hospeadado na raiz do
-  // site usando o escopo padrão
-  navigator.serviceWorker.register('./sw.js').then(function(registration) {
-    console.log('Service worker  registrado com sucesso:', registration);
-  }).catch(function(error) {
-    console.log('Falha ao Registrar o Service Worker:', error);
-  });
-} else {
-  console.log('Service workers não suportado!');
-}
+navigator.serviceWorker.register('./sw.js');
+
